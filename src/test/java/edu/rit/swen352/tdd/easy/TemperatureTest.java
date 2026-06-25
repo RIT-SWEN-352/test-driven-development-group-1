b27a742d14e1c3cef1d2e3fe73c0467a5b62ebbe
Alli H. <amh3192@rit.edu>
2026-06-24 17:20:08 -0700

    ctor : setting both values : create failing test

--- src/test/java/edu/rit/swen352/tdd/easy/TemperatureTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/TemperatureTest.java
@@ -2,6 +2,10 @@ package edu.rit.swen352.tdd.easy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import edu.rit.swen352.tdd.easy.Temperature.TemperatureUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,4 +14,12 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class TemperatureTest {
 
-}
+    @Test
+    @DisplayName("ctor with both values")
+    void ctor_1() {
+        final Temperature CuT = new Temperature(0, TemperatureUnit.FAHRENHEIT);
+        assertNotNull(CuT);
+    }
+    
+  }
+
