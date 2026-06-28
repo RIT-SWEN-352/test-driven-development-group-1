dc46d2e7942bc77a21da35d724f49a5ee7a55d07
GitIzzy72 <isaacebencorn.uni@gmail.com>
2026-06-28 18:07:21 -0400

    Mass Enum toString: toString should return their abbreviation - create passing code

--- src/main/java/edu/rit/swen352/tdd/hard/UnitEnum.java
+++ src/main/java/edu/rit/swen352/tdd/hard/UnitEnum.java
@@ -1,19 +1,22 @@
 package edu.rit.swen352.tdd.hard;
 
+import javax.lang.model.type.PrimitiveType;
 
 public interface UnitEnum {
     
     enum Mass {
-        KILOGRAM(1),
-        MILLIGRAM(.000001),
-        GRAM(.001),
-        OUNCE(0.0283495),
-        POUND(0.453592);
+        KILOGRAM(1,"kg"),
+        MILLIGRAM(.000001,"mg"),
+        GRAM(.001,"g"),
+        OUNCE(0.0283495,"oz"),
+        POUND(0.453592,"lb");
 
         private double kg;
+        private String abbr;
 
-        Mass(double kg){
+        Mass(double kg, String abbr){
             this.kg=kg;
+            this.abbr = abbr;
         }
 
         public double conversionRatio(Mass convertFrom) {
@@ -21,8 +24,7 @@ public interface UnitEnum {
         }
 
         public String toString(){
-            assert false : "Not yet implemented";
-            return null;
+            return abbr;
         }
 
     }
