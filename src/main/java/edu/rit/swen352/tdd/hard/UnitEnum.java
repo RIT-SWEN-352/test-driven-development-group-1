52d018faab67792c9e928b19d6087217250f953c
GitIzzy72 <isaacebencorn.uni@gmail.com>
2026-06-28 21:19:20 -0400

    implement the other enums the same way

--- src/main/java/edu/rit/swen352/tdd/hard/UnitEnum.java
+++ src/main/java/edu/rit/swen352/tdd/hard/UnitEnum.java
@@ -6,6 +6,9 @@ public interface UnitEnum {
     default double getConversionRatio(UnitEnum convertTo, UnitEnum convertFrom){
         return convertFrom.getBaseRatio()/convertTo.getBaseRatio();
     }
+    /**
+    * returns how many __this__ are in one __convertFrom__ unit
+     */
     default double getConversionRatio(UnitEnum convertFrom){
         return getConversionRatio(this, convertFrom);
     }
@@ -36,12 +39,59 @@ public interface UnitEnum {
 
     }
 
-    /*INCH, 
-        FEET,
-        YARD,
-        MILLIMETER,
-        CENTIMETER,
-        METER,
-        KILOMETER */
+    enum Length implements UnitEnum{
+        METER(1,"m"),
+        MILLIMETER(.001,"mm"),
+        CENTIMETER(.01,"cm"),
+        KILOMETER(1000,"km"),
+        INCH(0.0254,"in"), 
+        FEET(.3048,"ft"),
+        YARD(.9144,"yd"),
+        MILE(1609.34,"mi");
 
+        private double meter;
+        private String abbr;
+
+        Length(double meter, String abbr){
+            this.meter=meter;
+            this.abbr=abbr;
+        }
+
+        @Override
+        public double getBaseRatio() {
+            return meter;
+        }
+
+        @Override
+        public String toString() {
+            return abbr;
+        }
+
+    }
+
+    enum Time implements UnitEnum{
+        SECOND(1,"s"),
+        MINUTE(60,"min"),
+        HOUR(360,"hr")
+        ;
+
+        private double sec;
+        private String abbr;
+
+        Time(double sec, String abbr){
+            this.sec=sec;
+            this.abbr=abbr;
+        }
+
+        @Override
+        public double getBaseRatio() {
+            return sec;
+        }
+
+        @Override
+        public String toString() {
+            return abbr;
+        }
+
+    }
 }
