c2b2d8738eee549eee4dd779858873fee308c0f1
Christopher <chrisobando107@gmail.com>
2026-06-30 05:40:40 -0400

    ctor : constructor with supplied balance : create passing test

--- src/main/java/edu/rit/swen352/tdd/easy/SimpleBankAccount.java
+++ src/main/java/edu/rit/swen352/tdd/easy/SimpleBankAccount.java
@@ -22,5 +22,13 @@ package edu.rit.swen352.tdd.easy;
  * </ul>
  */
 public class SimpleBankAccount {
+    private float balance;
 
+    public SimpleBankAccount(Float initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
 }
