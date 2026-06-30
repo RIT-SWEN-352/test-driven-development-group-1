a8963478f90a9c55b68c1f618273ef17b0389788
Christopher <chrisobando107@gmail.com>
2026-06-30 05:10:57 -0400

    ctor : constructor with supplied balance : create failing test

--- src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
+++ src/test/java/edu/rit/swen352/tdd/easy/SimpleBankAccountTest.java
@@ -9,5 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
  * Test suite for the {@link SimpleBankAccount} component.
  */
 class SimpleBankAccountTest {
-
+    @Test
+    @DisplayName("Constructor: with supplied balance (ex:3000.75")
+    void testConstructorWithSuppliedBalance() {
+        float balance = 3000.75f;
+        SimpleBankAccount simpleAcount = new SimpleBankAccount(balance);
+        assertEquals(balance, simpleAcount.getBalance());
+    }
 }
